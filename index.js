18cfe11ebfcd73bbd00c5721b5a3691c291fedab
Sahil <sahilflash123@gmail.com>
2021-09-01 01:25:35 +0530

    Almost done with Front-end

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
-
-
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(express.static("public"));
 const port = 3000;
 
 app.get("/",(req, res)=>{
-    res.send("Hello");
+    res.sendFile(__dirname+"/index.html");
+})
+
+app.post("/",(req, res)=>{
+    console.log(req.body);
+    res.send("ok"); 
 })
 
 app.listen(port, () => {
